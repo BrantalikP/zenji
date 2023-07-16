332a9fd592e07c845837eb24338ab50564d1f3be
Petr Brantalík <grounipteam@gmail.com>
2023-07-16 12:43:33 +0200

    chore: zodios wip

--- src/features/home/screens/Home/Home.tsx
+++ src/features/home/screens/Home/Home.tsx
@@ -1,13 +1,17 @@
+import { ZodiosHooks } from '@zodios/react'
 import React from 'react'
 import { View } from 'react-native'
 
 import { styles } from './styles'
 
+import { apiHooks } from '~/features/api/wanikani/schema/zodios-client'
 import { Text } from '~/features/ui/components/Text'
 import { fontsStyles } from '~/features/ui/theme/fontsStyles'
 import { useStyles } from '~/features/ui/theme/hooks/useStyles'
 const Home = () => {
   const { container } = useStyles(styles)
+
+  const { data } = apiHooks.useGetUser()
   return (
     <View style={container}>
       <Text>Home</Text>
