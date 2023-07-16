eb0646b4a99bcbeba9b02bd150cfe17ac2423c8b
Petr Brantalík <grounipteam@gmail.com>
2023-07-16 13:33:19 +0200

    chore: update expo

--- src/features/home/screens/Home/Home.tsx
+++ src/features/home/screens/Home/Home.tsx
@@ -1,20 +1,21 @@
-import { ZodiosHooks } from '@zodios/react'
 import React from 'react'
 import { View } from 'react-native'
 
 import { styles } from './styles'
 
-import { apiHooks } from '~/features/api/wanikani/schema/zodios-client'
+import { useGetUser } from '~/features/api/wanikani/components'
 import { Text } from '~/features/ui/components/Text'
 import { fontsStyles } from '~/features/ui/theme/fontsStyles'
 import { useStyles } from '~/features/ui/theme/hooks/useStyles'
 const Home = () => {
   const { container } = useStyles(styles)
 
-  const { data } = apiHooks.useGetUser()
+  const { data } = useGetUser({})
+  const username = data?.data?.username
+
   return (
     <View style={container}>
-      <Text>Home</Text>
+      <Text>Hello {username}</Text>
       <Text style={{ ...fontsStyles.h0 }}>Header</Text>
       <Text style={{ ...fontsStyles.body }}>body</Text>
       <Text style={{ ...fontsStyles.bodySmallBold }}>body</Text>
