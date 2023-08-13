11ce659040cdf708f54230c5aac4f234d4c5d8c7
Petr Brantalík <grounipteam@gmail.com>
2023-08-13 13:23:17 +0200

    feat: userUser hook adeded

--- src/features/home/screens/Home/Home.tsx
+++ src/features/home/screens/Home/Home.tsx
@@ -4,19 +4,18 @@ import { View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { styles } from './styles'
+import { useUser } from '../../hook/useUser'
 
 import { Text } from '~/components'
 import { StorybookButton } from '~/components/StorybookButton'
-import { useGetUser } from '~/features/api/wanikani/components'
 const Home = () => {
   const { container } = useStyles(styles)
   const insets = useSafeAreaInsets()
-  const { data } = useGetUser({})
-  const username = data?.data?.username
+  const { user } = useUser()
 
   return (
     <View style={[container, { paddingTop: insets.top }]}>
-      <Text variant="h4">{`Welcome ${username}`}</Text>
+      <Text variant="h4">{`Welcome ${user?.username}`}</Text>
       <StorybookButton />
     </View>
   )
